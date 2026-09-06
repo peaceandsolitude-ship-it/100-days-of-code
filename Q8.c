0a120014a9983bcd29088674d50f50c9a05ff140
peaceandsolitude-ship-it <tulibanerjee993@gmail.com>
2026-09-06 10:56:13 -0700

    Create Q8.c

---
+++ Q8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+int n,i,s=0;
+printf("Enter the values of n=");
+scanf("%d/n");
+for(i=1;i<=n;i++)
+{
+s=s+i;
+}
+printf("Sum of the first natural numbers=%d\n");
+return 0;
+}
