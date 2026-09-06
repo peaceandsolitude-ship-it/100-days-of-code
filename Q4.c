4662ce4bc9aca90ae40d4337438ceeae59f26837
peaceandsolitude-ship-it <tulibanerjee993@gmail.com>
2026-09-06 10:22:57 -0700

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -2,19 +2,19 @@
 #define PI 3.14159
 int main()
 {
-    double radius, area, circumference;
-    printf("Enter the radius of the circle: ");
-    scanf("%lf", &radius);
-    if (radius < 0)
-    {
-        printf("Radius cannot be negative.\n");
-    }
-    else
-    {
-      area = PI * radius * radius;
-        circumference = 2 * PI * radius;
-        printf("Area of the circle = %.2lf\n", area);
-        printf("Circumference of the circle = %.2lf\n", circumference);
-    }
-    return 0;
+double radius, area, circumference;
+printf("Enter the radius of the circle: ");
+scanf("%lf", &radius);
+if (radius < 0)
+{
+printf("Radius cannot be negative.\n");
+}
+else
+{
+area = PI * radius * radius;
+circumference = 2 * PI * radius;
+printf("Area of the circle = %.2lf\n", area);
+printf("Circumference of the circle = %.2lf\n", circumference);
+}
+return 0;
 }
