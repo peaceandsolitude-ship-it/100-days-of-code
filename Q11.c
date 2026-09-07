0a0b1c75ef3416572198b9251d9d40e5ef72dc33
peaceandsolitude-ship-it <tulibanerjee993@gmail.com>
2026-09-07 10:16:39 -0700

    Create Q11.c

---
+++ Q11.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+  int main();
+{
+int n,even,odd;
+printf("Enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+  printf("It's an even number");
+}
+else
+{
+  printf("It's an odd number");
+}
+return 0;
+}
