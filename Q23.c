ce2378ab9212c3aa3df561c2c57a5a1c20a0fb22
peaceandsolitude-ship-it <tulibanerjee993@gmail.com>
2026-09-11 12:22:59 -0700

    Update Q23.c

--- Q23.c
+++ Q23.c
@@ -1,30 +1,26 @@
-include <stdio.h>
-int main() 
+#include<stdio.h>
+int main()
 {
-    float cp, sp;
-    printf("Enter Cost Price: ");
-    scanf("%f", &cp);
-    printf("Enter Selling Price: ");
-    scanf("%f", &sp);
-    if (cp <= 0 || sp < 0)
-{
-        printf("Invalid! Prices must be positive.");
+    float units, bill = 0;
+    printf("Enter the number of units consumed: ");
+    scanf("%f", &units);
+    if (units <= 100)
+    {
+        bill = units * 5;
     }
-    else if (sp > cp)
-{
-        float profit = sp - cp;
-        float percent = (profit / cp) * 100;
-        printf("Profit of %.2f (%.2f%%)", profit, percent);
+    else if (units <= 200)
+    {
+        bill = (100 * 5) + ((units - 100) * 7);
     }
-    else if (cp > sp)
-{
-        float loss = cp - sp;
-        float percent = (loss / cp) * 100;
-        printf("Loss of %.2f (%.2f%%)", loss, percent);
+    else if (units <= 300)
+    {
+        bill = (100 * 5) + (100 * 7) + ((units - 200) * 10);
+    }
+    else
+    {
+        bill = (100 * 5) + (100 * 7) + (100 * 10) + ((units - 300) * 12);
+    }
+    printf("Electricity Bill = ₹ %.2f", bill);
+   return 0;
 }
-    else 
-{
-        printf("No Profit, No Loss.");
-    }
-    return 0;
-}
+      
