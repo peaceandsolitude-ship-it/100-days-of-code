801c3ce689ea60c94ed2ea5d9c5492815aa8fe08
peaceandsolitude-ship-it <tulibanerjee993@gmail.com>
2026-09-12 03:50:21 -0700

    Create Q43.c

---
+++ Q43.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int num, temp, digit, fact, sum=0, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num;
+    while(temp > 0)
+    {
+        digit = temp % 10;
+        fact = 1;
+        for(i=1; i<=digit; i++)
+        {
+            fact = fact * i;
+        }
+        sum = sum + fact;
+        temp = temp / 10;
+    }
+    if(sum == num)
+    {
+        printf("%d is a Strong Number", num);
+    }
+    else
+    {
+        printf("%d is not a Strong Number", num);
+    }
+    return 0;
+}
